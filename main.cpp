1bc38d7c8e3fa3df977b4a4cc93f5e8149c67267
Alyssa Nielson <alyssanielson@Alyssas-MacBook-Air.local>
2021-06-12 20:08:30 -0600

    added board

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -25,6 +26,76 @@ int main() {
             // if player places 3 sequential X's, they win. Repeat
             // player moves, computer selects random unoccupied space (alternate character placement). repeat
             // if occupied space is chosen, notify user, and have them enter different space. Continue
+
+
+
+        cout << " - - - \n - - - \n - - -" << endl;
+        cout << "1. Top left" << endl;
+        cout << "2. Top middle" << endl;
+        cout << "3. Top right" << endl;
+        cout << "4. Center left" << endl;
+        cout << "5. Middle" << endl;
+        cout << "6. Center right" << endl;
+        cout << "7. Bottom left" << endl;
+        cout << "8. Bottom middle" << endl;
+        cout << "9. Bottom right" << endl;
+        int t1;
+        cin >> t1;
+            if (t1 == 1) {
+                cout << " X - - \n - - - \n - - -" << endl;
+            }
+            else if (t1 == 2) {
+                cout << " - X - \n - - - \n - - -" << endl;
+            }
+            else if (t1 == 3) {
+                cout << " - - X \n - - - \n - - -" << endl;
+            }
+            else if (t1 == 4) {
+                cout << " - - - \n X - - \n - - -" << endl;
+            }
+            else if (t1 == 5) {
+                cout << " - - - \n - X - \n - - -" << endl;
+            }
+            else if (t1 == 6) {
+                cout << " - - - \n - - X \n - - -" << endl;
+            }
+            else if (t1 == 7) {
+                cout << " - - - \n - - - \n X - -" << endl;
+            }
+            else if (t1 == 8) {
+                cout << " - - - \n - - - \n - X -" << endl;
+            }
+            else if (t1 == 9) {
+                cout << " - - - \n - - - \n - - X" << endl;
+            }
+        cout << "Computer's turn (O)" << endl;
+            // From 1 to 9
+        int ct1;
+        ct1 = 1 + rand() % 9;
+        while (ct1 == t1) {
+            ct1 = 1 + rand() % 9;
+        }
+        if (ct1 == 0) {
+            cout << " O - - \n - - - \n - - -" << endl;
+        } else if (ct1 == 1) {
+            cout << " - O - \n - - - \n - - -" << endl;
+        } else if (ct1 == 2) {
+            cout << " - - O \n - - - \n - - -" << endl;
+        } else if (ct1 == 3) {
+            cout << " - - - \n O - - \n - - -" << endl;
+        } else if (ct1 == 4) {
+            cout << " - - - \n - O - \n - - -" << endl;
+        } else if (ct1 == 5) {
+            cout << " - - - \n - - O \n - - -" << endl;
+        } else if (ct1 == 6) {
+            cout << " - - - \n - - - \n O - -" << endl;
+        } else if (ct1 == 7) {
+            cout << " - - - \n - - - \n - O -" << endl;
+        } else if (ct1 == 8) {
+            cout << " - - - \n - - - \n - - O" << endl;
+        }
+
+
             cout << "You won!" << endl << endl;
             cout << "   Play again?" << endl;
             cout << "       1. Yes\n       2. No\n" << endl;
